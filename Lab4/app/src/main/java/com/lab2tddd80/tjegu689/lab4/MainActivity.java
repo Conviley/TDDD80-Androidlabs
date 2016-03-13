1bfced304d9cffd216242136b21469117a76237d
Tjelvar Guo <tjegu689@student.liu.se>
2016-03-13 12:03:20 +0100

    skapat lab4

---
+++ Lab4/app/src/main/java/com/lab2tddd80/tjegu689/lab4/MainActivity.java
@@ -0,0 +1,13 @@
+package com.lab2tddd80.tjegu689.lab4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
